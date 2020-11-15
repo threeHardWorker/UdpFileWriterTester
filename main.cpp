458fb6aab3d881fa75d541d877bbc576461573b9
yanghuatao <yanghuatao@xdf.cn>
2020-11-15 23:20:03 +0800

    增加测试解包功能

--- main.cpp
+++ main.cpp
@@ -6,10 +6,41 @@
 #include <QDateTime>
 #include <QFile>
 
+//#define TEST_UNPACH
+
+bool testUnpack(const QString &fileName)
+{
+    QFile file(fileName);
+    if (file.open(QFile::ReadOnly))
+    {
+        while (!file.atEnd())
+        {
+            int i = sizeof(uint32_t);
+            char len[sizeof(uint32_t)+1] = {0};
+            file.read(len, sizeof(uint32_t));
+            uint32_t iLen = *(uint32_t*)(&len);
+
+            char time[13] = {0};
+            file.read(time, 13);
+            QByteArray gzip = file.read(iLen-13);
+            QFile fileGzip(QString(time) + ".gz");
+            if (fileGzip.open(QFile::WriteOnly))
+            {
+                fileGzip.write(gzip, iLen-13);
+                fileGzip.close();
+            }
+        }
+    }
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+#ifdef TEST_UNPACH
+    testUnpack(QCoreApplication::applicationDirPath() + "/2020-11-15");
+#else
     if (argc < 3)
     {
         qDebug()<<"exaple:testUdpClient 127.0.0.1 8899"<<endl;
@@ -20,7 +51,7 @@ int main(int argc, char *argv[])
     qint16 port = QString(argv[2]).toShort();
 
     QUdpSocket qus;
-    for(int i=0; i<20; ++i)
+    for(int i=0; i<5; ++i)
     {
         QByteArray msg = "market.BTC-USDT.depth.step01";
         msg = msg.leftJustified(64, ' ', true);
@@ -40,6 +71,7 @@ int main(int argc, char *argv[])
             break;
         }
     }
+#endif
 
     return a.exec();
 }
